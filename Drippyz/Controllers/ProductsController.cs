813f3df3f49078b7735da9374ffb9b22afae339e
Gillian Mc Quillan <X00179268@myTUDublin.ie>
2022-04-23 11:06:35 +0100

    Product and store MVC added basic display no functionality added.

---
+++ Drippyz/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿
+using Drippyz.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Drippyz.Controllers
+{
+    public class ProductsController : Controller
+    {
+
+        //declare app db context 
+        private readonly AppDbContext _context;
+        //constructor
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //default action result 
+        //var data = return products in this controller and also  pass the data as a parameter to the view
+        //Asynchronous method with parameters
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _context.Products.ToListAsync();
+            return View(allProducts);
+
+        }
+        /*
+        //Action Get request 
+        public async Task<IActionResult> Details(int id)
+        {
+            var productDetail = await _service.GetByIdAsync(id);
+            return View(productDetail);
+        }
+
+        //Get Product/Create
+        public IActionResult Create()
+        {
+            ViewData["Welcome"] = "Welcome to our store";
+            ViewBag.Description = "This is the store description.";
+            return View();
+        }*/
+    }
+}
---
+++ Drippyz/Controllers/StoresController.cs
@@ -0,0 +1,73 @@
+﻿using Drippyz.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Drippyz.Controllers
+{
+    public class StoresController : Controller
+    {
+        // //inject IStore service 
+        private readonly AppDbContext _context;
+        //constructor
+        public StoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //default action result 
+        //var data = return store in this controller and also  pass the data as a parameter to the view
+        public async Task<IActionResult> Index()
+        {
+            var allStores = await _context.Stores.ToListAsync();
+            return View(allStores);
+        }
+        /*
+        //Get Request Create View 
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Glyph,Name,Description")] Store store)
+        {
+            if (!ModelState.IsValid) return View(store);
+            await _service.AddAsync(store);
+            return RedirectToAction(nameof(Index));
+        }
+        //Get: Stores Details 
+        public async Task<IActionResult> Details(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+            return View(storeDetails);
+        }
+        //Get the store details edit Id
+        //Post request after the details are updated
+        public async Task<IActionResult> Edit(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+            return View(storeDetails);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Glyph,Name,Description")] Store store)
+        {
+            if (!ModelState.IsValid) return View(store);
+            await _service.AddAsync(store);
+            return RedirectToAction(nameof(Index));
+        }
+        //Store Delete
+        [HttpPost, ActionName("Delete")]
+        //of store exists in database call the delete confirm method
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var storeDetails = await _service.GetByIdAsync(id);
+            if (storeDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }*/
+    }
+}
+
